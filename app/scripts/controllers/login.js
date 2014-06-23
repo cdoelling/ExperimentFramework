ee0533d8e464ad069df2695216eebb1d83c786fe
Charles Ferentchak <charles@rallydev.com>
2014-06-23 17:15:55 -0600

    updates location after login

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,11 +1,14 @@
 'use strict';
 
 angular.module('experimentFrameworkApp')
-.controller('LoginCtrl', function ($scope,loginService,userService) {
+.controller('LoginCtrl', function ($scope,loginService,userService,$location) {
   $scope.submit = function() {
     loginService($scope.username,$scope.password)
     .then(function(){
-      userService.getUser();
+      userService.getUser()
+      .then(function(){
+        $location.url('/');
+      });
     });
 
   };
