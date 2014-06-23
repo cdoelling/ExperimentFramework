2a4e27f1d80dfd9a26f2cfc085ba5bed5c0696e7
Charles Ferentchak <charles@rallydev.com>
2014-06-23 15:21:37 -0600

    login stores key

--- app/scripts/controllers/login.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('experimentFrameworkApp')
-.controller('LoginCtrl', function ($scope,userService) {
-  $scope.submit = function() {
-    userService.login($scope.username,$scope.password);
-  };
-});
---
+++ app/scripts/services/loginService.js
@@ -0,0 +1,23 @@
+'use strict';
+angular.module('experimentFrameworkApp')
+.factory('loginService', function($http) {
+  function login(username,password){
+    var auth = btoa(username + ':' + password);
+    return $http({
+      method:'GET',
+      url:'https://rally1.rallydev.com/slm/webservice/v2.0/security/authorize',
+      headers:{
+        'Authorization': 'Basic ' +  auth,
+        'X-Requested-By': 'Rally',
+        'X-RallyIntegrationName': 'Rally Experiment Framework'
+      },
+      withCredentials:true,
+      responseType:'json'
+    })
+    .success(function(data){
+      var token = data.OperationResult.SecurityToken;
+      window.sessionStorage.setItem('securityToken', token ? token : '');
+    });
+  }
+  return login;
+});
