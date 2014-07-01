d9d29f93007ed25e8387677106d135d3b0def995
Charles Ferentchak <charles@rallydev.com>
2014-07-01 12:03:10 -0600

    Close #4

--- app/scripts/controllers/experiment.js
+++ app/scripts/controllers/experiment.js
@@ -1,8 +1,18 @@
 'use strict';
-
 angular.module('experimentFrameworkApp')
 .controller('ExperimentCtrl', function ($scope,$routeParams,preferenceService) {
   $scope.blur = function(field){
-    preferenceService.saveExperimentPart($routeParams.id,field,$scope[field]);
+    var objectId = $scope.ids[field];
+    preferenceService.saveExperimentPart($routeParams.id,field,$scope[field],objectId);
   };
+  preferenceService.getExperimentById($routeParams.id)
+  .then(function(experimentParts){
+      $scope.ids = $scope.ids || {};
+    _.each(experimentParts,function(part){
+      var field = part.Name.split(' ')[0].split('-')[1];
+      $scope[field] = part.Value;
+      $scope.ids[field] = part.ObjectID;
+
+    });
+  });
 });
--- app/scripts/services/preferenceService.js
+++ app/scripts/services/preferenceService.js
@@ -7,6 +7,25 @@
   angular.module('experimentFrameworkApp')
   .factory('preferenceService', function($http,userService,$q) {
     var service = {
+      getExperimentById:function(id){
+        var deferred = $q.defer();
+        $http({
+          method:'GET',
+          url: 'https://rally1.rallydev.com/slm/webservice/v2.0/preference?pretty=true&fetch=true&query=(Name contains "'+id+'")',
+          headers:headers,
+          withCredentials:true,
+          responseType:'json'
+        })
+        .success(function(data){
+          var d = data.QueryResult.Results;
+          deferred.resolve(d);
+        })
+        .catch(function(err){
+          deferred.reject(err);
+        });
+        return deferred.promise;
+
+      },
       getExperiments:function(){
         var deferred = $q.defer();
         $http({
@@ -25,8 +44,9 @@
         });
         return deferred.promise;
       },
-      saveExperimentPart:function(experimentId,name,data){
+      saveExperimentPart:function(experimentId,name,data,objectId){
         var key = 'experiment-' + name + ' '+experimentId;
+        var endpoint = objectId ? objectId : 'create';
         return $http({
           method:'POST',
           data:{
@@ -36,7 +56,7 @@
               Value:data
             }
           },
-          url: 'https://rally1.rallydev.com/slm/webservice/v2.0/preference/create',
+          url: 'https://rally1.rallydev.com/slm/webservice/v2.0/preference/' + endpoint,
           params:{
             key:window.sessionStorage.securityToken
           },
