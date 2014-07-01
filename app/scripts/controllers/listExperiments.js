e6c50ce8ee4df3ff65f5beeb11ed86bea61906f7
Charles Ferentchak <charles@rallydev.com>
2014-07-01 10:51:41 -0600

    Close #3

--- app/scripts/controllers/listExperiments.js
+++ app/scripts/controllers/listExperiments.js
@@ -4,6 +4,9 @@ angular.module('experimentFrameworkApp')
 .controller('ListExperimentsCtrl', function ($scope,preferenceService) {
   preferenceService.getExperiments()
   .then(function(experiments){
-    $scope.things = experiments;
+    _.each(experiments,function(e){
+      e._id = e.Name.split(' ')[1];
+    });
+    $scope.experiments = experiments;
   });
 });
